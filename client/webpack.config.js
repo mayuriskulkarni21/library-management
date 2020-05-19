fd5bfe6dd87b8380e881c32b04f5e4a94b14e973
Mayuri S Kulkarni <mayu.s.kulkarni21@gmail.com>
2020-05-19 23:46:34 +0530

    React app setup

---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.join(__dirname, '/bundle'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+}
